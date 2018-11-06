8a7b041ac1a2dd375250d7df3d1d6ae26d9d44dd
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-07 00:12:30 +0100

    Provjera ispravnosti pozicije ChessPiece

--- src/ba/unsa/etf/rpr/ChessPiece.java
+++ src/ba/unsa/etf/rpr/ChessPiece.java
@@ -1,15 +1,34 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+import java.util.ArrayList.*;
+
 public abstract class ChessPiece {
 
     public static enum Color {
         BLACK, WHITE
     }
 
+    private String ispravni = "ABCDEFGHabcdefgh";
+
     protected String position;
     protected Color color;
 
+    private boolean ispravnaPozicija(String position) {
+        if (position.length() != 2)
+            return false;
+        if (!ispravni.contains(position.charAt(0)))
+            return false;
+        if (!Character.isDigit(position.charAt(1)))
+            return false;
+        if (position.charAt(1) < 1 || position.charAt(1) > 8)
+            return false;
+        return false;
+    }
+
     public ChessPiece(String position, Color color) {
+        if (!ispravnaPozicija(position))
+            throw new IllegalArgumentException("Pozicija je neispravnog formata ili je van table!");
         this.position = position;
         this.color = color;
     }
@@ -21,6 +40,9 @@ public abstract class ChessPiece {
         return color;
     }
     public void move(String position) {
+        if (!ispravnaPozicija(position))
+            throw new IllegalArgumentException("Pozicija je neispravnog formata ili je van table!");
+
         this.position = position;
     }
 }
