005baca42984795c2baece894633364573b21cb6
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-07 03:34:50 +0100

    Ostale klase

---
+++ src/ba/unsa/etf/rpr/Rook.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.rpr;
+
+public class Rook {
+
+    public Rook(String position, ChessPiece.Color color) {
+
+    }
+
+    public String getPosition() {
+
+    }
+    public ChessPiece.Color getColor() {
+
+    }
+    public void move(String position) {
+
+    }
+}
