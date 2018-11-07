4f54970018810964f8950a5f35d3b7373c239417
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-07 11:58:34 +0100

    Bishop prepravka

--- src/ba/unsa/etf/rpr/Bishop.java
+++ src/ba/unsa/etf/rpr/Bishop.java
@@ -1,6 +1,6 @@
 package ba.unsa.etf.rpr;
 
-public class Bishop {
+public class Bishop extends ChessPiece {
 
     public Bishop(String position, ChessPiece.Color color) {
         super(position, color);
