1fb4a3cd1e4b29bb8afec5ee2efa2898d763c3d6
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-07 11:32:40 +0100

    Konstruktor

--- src/ba/unsa/etf/rpr/Board.java
+++ src/ba/unsa/etf/rpr/Board.java
@@ -1,16 +1,47 @@
 package ba.unsa.etf.rpr;
 
+import com.sun.xml.internal.bind.v2.runtime.reflect.ListTransducedAccessorImpl;
+
 import java.util.ArrayList;
 
 public class Board {
 
-    private ArrayList<ChessPiece> listaCrnih;
-    private ArrayList<ChessPiece> listaBijelih;
+    private ArrayList<ChessPiece> listaFigura;
 
     public Board() {
-        listaCrnih = new ArrayList<>();
-        listaBijelih = new ArrayList<>();
-        //listaCrnih.add(new Rook("A1", ChessPiece.Color.WHITE));
+        listaFigura = new ArrayList<>();
+        listaFigura.add(new Rook("A1", ChessPiece.Color.WHITE));
+        listaFigura.add(new Knight("B1", ChessPiece.Color.WHITE));
+        listaFigura.add(new Bishop("C1", ChessPiece.Color.WHITE));
+        listaFigura.add(new Queen("D1", ChessPiece.Color.WHITE));
+        listaFigura.add(new King("E1", ChessPiece.Color.WHITE));
+        listaFigura.add(new Bishop("F1", ChessPiece.Color.WHITE));
+        listaFigura.add(new Knight("G1", ChessPiece.Color.WHITE));
+        listaFigura.add(new Rook("H1", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("A2", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("B2", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("C2", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("D2", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("E2", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("F2", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("G2", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("H2", ChessPiece.Color.WHITE));
+        listaFigura.add(new Rook("A8", ChessPiece.Color.WHITE));
+        listaFigura.add(new Knight("B8", ChessPiece.Color.WHITE));
+        listaFigura.add(new Bishop("C8", ChessPiece.Color.WHITE));
+        listaFigura.add(new Queen("D8", ChessPiece.Color.WHITE));
+        listaFigura.add(new King("E8", ChessPiece.Color.WHITE));
+        listaFigura.add(new Bishop("F8", ChessPiece.Color.WHITE));
+        listaFigura.add(new Knight("G8", ChessPiece.Color.WHITE));
+        listaFigura.add(new Rook("H8", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("A7", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("B7", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("C7", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("D7", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("E7", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("F7", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("G7", ChessPiece.Color.WHITE));
+        listaFigura.add(new Pawn("H7", ChessPiece.Color.WHITE));
     }
 
     public void move(Class type, ChessPiece.Color color, String position) {
