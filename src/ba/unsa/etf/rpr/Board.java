8ed5f9d18f53df532302b7941cd391ec59d690fc
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-07 03:33:48 +0100

    Board - prepravka

--- src/ba/unsa/etf/rpr/Board.java
+++ src/ba/unsa/etf/rpr/Board.java
@@ -1,13 +1,17 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+
 public class Board {
 
-    /*private
-    private
+    private ArrayList<ChessPiece> listaCrnih;
+    private ArrayList<ChessPiece> listaBijelih;
 
     public Board() {
-
-    }*/
+        listaCrnih = new ArrayList<>();
+        listaBijelih = new ArrayList<>();
+        listaCrnih.add(new Rook("A1", ChessPiece.Color.WHITE));
+    }
 
     public void move(Class type, ChessPiece.Color color, String position) {
 
