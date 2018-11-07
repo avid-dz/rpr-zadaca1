3e8984f1695d77fd4f89d84a2a8d057aabb59316
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-07 11:55:20 +0100

    Konstruktor Pawn

--- src/ba/unsa/etf/rpr/Pawn.java
+++ src/ba/unsa/etf/rpr/Pawn.java
@@ -2,5 +2,11 @@ package ba.unsa.etf.rpr;
 
 public class Pawn {
 
+    public Pawn(String position, ChessPiece.Color color) {
+        super(position, color);
+    }
 
+    public void move(String position) {
+
+    }
 }
