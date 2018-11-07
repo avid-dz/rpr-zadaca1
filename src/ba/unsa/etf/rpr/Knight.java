6ad01e462f89b76bf2f0befdfd48f15e02b1eede
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-07 11:58:45 +0100

    Knight prepravka

--- src/ba/unsa/etf/rpr/Knight.java
+++ src/ba/unsa/etf/rpr/Knight.java
@@ -1,6 +1,6 @@
 package ba.unsa.etf.rpr;
 
-public class Knight {
+public class Knight extends ChessPiece {
 
     public Knight(String position, ChessPiece.Color color) {
         super(position, color);
