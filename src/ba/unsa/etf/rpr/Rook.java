88ebd6098ad0de62d380383e5f9905e36fab87e1
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-07 11:55:49 +0100

    Konstruktor Rook

--- src/ba/unsa/etf/rpr/Rook.java
+++ src/ba/unsa/etf/rpr/Rook.java
@@ -3,15 +3,9 @@ package ba.unsa.etf.rpr;
 public class Rook {
 
     public Rook(String position, ChessPiece.Color color) {
-
+        super(position, color);
     }
 
-    public String getPosition() {
-
-    }
-    public ChessPiece.Color getColor() {
-
-    }
     public void move(String position) {
 
     }
