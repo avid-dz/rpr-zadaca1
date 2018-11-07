71ec2ee99f43939aa738ddf3c43b1ee70ae1e9d0
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-07 11:55:07 +0100

    Konstruktor Knight

--- src/ba/unsa/etf/rpr/Knight.java
+++ src/ba/unsa/etf/rpr/Knight.java
@@ -2,5 +2,11 @@ package ba.unsa.etf.rpr;
 
 public class Knight {
 
+    public Knight(String position, ChessPiece.Color color) {
+        super(position, color);
+    }
 
+    public void move(String position) {
+
+    }
 }
