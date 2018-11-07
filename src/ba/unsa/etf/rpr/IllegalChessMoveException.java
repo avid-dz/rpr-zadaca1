323220f188ee799d7334d502a4bfb44646ed27d9
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-07 12:42:02 +0100

    Izuzetak prepravka

--- src/ba/unsa/etf/rpr/IllegalChessMoveException.java
+++ src/ba/unsa/etf/rpr/IllegalChessMoveException.java
@@ -1,6 +1,6 @@
 package ba.unsa.etf.rpr;
 
-public class IllegalChessMoveException extends Exception {
+public class IllegalChessMoveException extends RuntimeException {
 
     public IllegalChessMoveException(String porukaIzuzetka) {
         super(porukaIzuzetka);
