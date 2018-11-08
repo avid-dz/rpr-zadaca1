1e29e1d18a18549fe80029cb55e7a0023a22a0bd
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-08 01:08:20 +0100

    King provjera

--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -5,8 +5,10 @@ import java.lang.Math;
 public class King extends ChessPiece {
 
     protected boolean ispravnoZaTuFiguru(String position) {
-        if (Math.abs(ChessPiece.slovnaKoordinata(position) - ChessPiece.slovnaKoordinata(this.position)) > 1) return false;
-        
+        if (Math.abs(ChessPiece.slovnaKoordinata(position) - ChessPiece.slovnaKoordinata(this.position)) > 1)
+            return false;
+        if (Math.abs(ChessPiece.brojevnaKoordinata(position) - ChessPiece.brojevnaKoordinata(this.position)) > 1)
+            return false;
         return true;
     }
 
