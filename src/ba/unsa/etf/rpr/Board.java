6760cf1b7ab1553baa08a4e12efde8fc6cfda565
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-08 03:43:10 +0100

    Implementujem pomjeranje u Board

--- src/ba/unsa/etf/rpr/Board.java
+++ src/ba/unsa/etf/rpr/Board.java
@@ -4,57 +4,66 @@ import java.util.ArrayList;
 
 public class Board {
 
-    private ArrayList<ChessPiece> listaFigura;
+    private ArrayList<ChessPiece> piecesList;
 
     public Board() {
-        listaFigura = new ArrayList<>();
-        listaFigura.add(new Rook("A1", ChessPiece.Color.WHITE));
-        listaFigura.add(new Knight("B1", ChessPiece.Color.WHITE));
-        listaFigura.add(new Bishop("C1", ChessPiece.Color.WHITE));
-        listaFigura.add(new Queen("D1", ChessPiece.Color.WHITE));
-        listaFigura.add(new King("E1", ChessPiece.Color.WHITE));
-        listaFigura.add(new Bishop("F1", ChessPiece.Color.WHITE));
-        listaFigura.add(new Knight("G1", ChessPiece.Color.WHITE));
-        listaFigura.add(new Rook("H1", ChessPiece.Color.WHITE));
-        listaFigura.add(new Pawn("A2", ChessPiece.Color.WHITE));
-        listaFigura.add(new Pawn("B2", ChessPiece.Color.WHITE));
-        listaFigura.add(new Pawn("C2", ChessPiece.Color.WHITE));
-        listaFigura.add(new Pawn("D2", ChessPiece.Color.WHITE));
-        listaFigura.add(new Pawn("E2", ChessPiece.Color.WHITE));
-        listaFigura.add(new Pawn("F2", ChessPiece.Color.WHITE));
-        listaFigura.add(new Pawn("G2", ChessPiece.Color.WHITE));
-        listaFigura.add(new Pawn("H2", ChessPiece.Color.WHITE));
-        listaFigura.add(new Rook("A8", ChessPiece.Color.BLACK));
-        listaFigura.add(new Knight("B8", ChessPiece.Color.BLACK));
-        listaFigura.add(new Bishop("C8", ChessPiece.Color.BLACK));
-        listaFigura.add(new Queen("D8", ChessPiece.Color.BLACK));
-        listaFigura.add(new King("E8", ChessPiece.Color.BLACK));
-        listaFigura.add(new Bishop("F8", ChessPiece.Color.BLACK));
-        listaFigura.add(new Knight("G8", ChessPiece.Color.BLACK));
-        listaFigura.add(new Rook("H8", ChessPiece.Color.BLACK));
-        listaFigura.add(new Pawn("A7", ChessPiece.Color.BLACK));
-        listaFigura.add(new Pawn("B7", ChessPiece.Color.BLACK));
-        listaFigura.add(new Pawn("C7", ChessPiece.Color.BLACK));
-        listaFigura.add(new Pawn("D7", ChessPiece.Color.BLACK));
-        listaFigura.add(new Pawn("E7", ChessPiece.Color.BLACK));
-        listaFigura.add(new Pawn("F7", ChessPiece.Color.BLACK));
-        listaFigura.add(new Pawn("G7", ChessPiece.Color.BLACK));
-        listaFigura.add(new Pawn("H7", ChessPiece.Color.BLACK));
+        piecesList = new ArrayList<>();
+        piecesList.add(new Rook("A1", ChessPiece.Color.WHITE));
+        piecesList.add(new Knight("B1", ChessPiece.Color.WHITE));
+        piecesList.add(new Bishop("C1", ChessPiece.Color.WHITE));
+        piecesList.add(new Queen("D1", ChessPiece.Color.WHITE));
+        piecesList.add(new King("E1", ChessPiece.Color.WHITE));
+        piecesList.add(new Bishop("F1", ChessPiece.Color.WHITE));
+        piecesList.add(new Knight("G1", ChessPiece.Color.WHITE));
+        piecesList.add(new Rook("H1", ChessPiece.Color.WHITE));
+        piecesList.add(new Pawn("A2", ChessPiece.Color.WHITE));
+        piecesList.add(new Pawn("B2", ChessPiece.Color.WHITE));
+        piecesList.add(new Pawn("C2", ChessPiece.Color.WHITE));
+        piecesList.add(new Pawn("D2", ChessPiece.Color.WHITE));
+        piecesList.add(new Pawn("E2", ChessPiece.Color.WHITE));
+        piecesList.add(new Pawn("F2", ChessPiece.Color.WHITE));
+        piecesList.add(new Pawn("G2", ChessPiece.Color.WHITE));
+        piecesList.add(new Pawn("H2", ChessPiece.Color.WHITE));
+        piecesList.add(new Rook("A8", ChessPiece.Color.BLACK));
+        piecesList.add(new Knight("B8", ChessPiece.Color.BLACK));
+        piecesList.add(new Bishop("C8", ChessPiece.Color.BLACK));
+        piecesList.add(new Queen("D8", ChessPiece.Color.BLACK));
+        piecesList.add(new King("E8", ChessPiece.Color.BLACK));
+        piecesList.add(new Bishop("F8", ChessPiece.Color.BLACK));
+        piecesList.add(new Knight("G8", ChessPiece.Color.BLACK));
+        piecesList.add(new Rook("H8", ChessPiece.Color.BLACK));
+        piecesList.add(new Pawn("A7", ChessPiece.Color.BLACK));
+        piecesList.add(new Pawn("B7", ChessPiece.Color.BLACK));
+        piecesList.add(new Pawn("C7", ChessPiece.Color.BLACK));
+        piecesList.add(new Pawn("D7", ChessPiece.Color.BLACK));
+        piecesList.add(new Pawn("E7", ChessPiece.Color.BLACK));
+        piecesList.add(new Pawn("F7", ChessPiece.Color.BLACK));
+        piecesList.add(new Pawn("G7", ChessPiece.Color.BLACK));
+        piecesList.add(new Pawn("H7", ChessPiece.Color.BLACK));
     }
 
     public void move(Class type, ChessPiece.Color color, String position) {
-        String staraPozicija;
-        for (ChessPiece chessPiece : listaFigura) {
-            if (chessPiece.getClass() == type && chessPiece.getColor() == color) {
-                staraPozicija = new String(chessPiece.getPosition());
+        String olderPosition = "";
+        boolean isFound = false;
+        ChessPiece movablePiece = null;
+        for (ChessPiece chessPiece : piecesList) {
+            if (chessPiece.getClass().equals(type) && chessPiece.getColor() == color) {
                 try {
+                    olderPosition = chessPiece.getPosition();
                     chessPiece.move(position);
-                } catch(Exception e) {
-
-                }
+                    isFound = true;
+                    movablePiece = chessPiece;
+                    break;
+                } catch(Exception e) {}
             }
         }
-
+        if (!isFound) throw new IllegalChessMoveException("No legal chess pieces found for that move!");
+        for (ChessPiece chessPiece : piecesList) {
+            if (chessPiece.getPosition().equals(position) && chessPiece.getColor() == movablePiece.getColor()) {
+                movablePiece.move(olderPosition);
+                throw new IllegalChessMoveException("Field already taken by the same color!");
+            }
+        }
     }
     public void move(String oldPosition, String newPosition) {
 
