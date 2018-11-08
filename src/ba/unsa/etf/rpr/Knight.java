cc5b4e76f8c75ed1f245949aa41f5b7dd4ee8c8f
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-08 02:10:40 +0100

    Knight provjera uradjena

--- src/ba/unsa/etf/rpr/Knight.java
+++ src/ba/unsa/etf/rpr/Knight.java
@@ -3,6 +3,15 @@ package ba.unsa.etf.rpr;
 public class Knight extends ChessPiece {
 
     protected boolean ispravnoZaTuFiguru(String position) {
+        if (!(Math.abs(ChessPiece.slovnaKoordinata(position)
+                - ChessPiece.slovnaKoordinata(this.position)) == 2
+                && Math.abs(ChessPiece.brojevnaKoordinata(position)
+                - ChessPiece.brojevnaKoordinata(this.position)) == 1)
+                && !(Math.abs(ChessPiece.brojevnaKoordinata(position)
+                - ChessPiece.brojevnaKoordinata(this.position)) == 2
+                && Math.abs(ChessPiece.slovnaKoordinata(position)
+                - ChessPiece.slovnaKoordinata(this.position)) == 1))
+            return false;
         return true;
     }
 
