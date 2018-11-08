6d7433ab5026f94de0d6fc12c2282b7762f5c9dd
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-08 02:28:57 +0100

    Pawn provjera uradjena

--- src/ba/unsa/etf/rpr/Pawn.java
+++ src/ba/unsa/etf/rpr/Pawn.java
@@ -3,6 +3,14 @@ package ba.unsa.etf.rpr;
 public class Pawn extends ChessPiece {
 
     protected boolean ispravnoZaTuFiguru(String position) {
+        if (Math.abs(ChessPiece.slovnaKoordinata(position) - ChessPiece.slovnaKoordinata(this.position)) > 1)
+            return false;
+        if (Math.abs(ChessPiece.brojevnaKoordinata(position) - ChessPiece.brojevnaKoordinata(this.position)) > 2)
+            return false;
+        if ((ChessPiece.brojevnaKoordinata(this.position) == 4 || ChessPiece.brojevnaKoordinata(this.position) == 5)
+                && (Math.abs(ChessPiece.brojevnaKoordinata(position)
+                - ChessPiece.brojevnaKoordinata(this.position)) > 1))
+            return false;
         return true;
     }
 
