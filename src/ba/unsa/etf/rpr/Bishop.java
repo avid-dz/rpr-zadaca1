ff59b300f985c93be2e6f866c982ba5bac575a0a
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-08 01:41:52 +0100

    Bishop provjera napravljena

--- src/ba/unsa/etf/rpr/Bishop.java
+++ src/ba/unsa/etf/rpr/Bishop.java
@@ -3,6 +3,9 @@ package ba.unsa.etf.rpr;
 public class Bishop extends ChessPiece {
 
     protected boolean ispravnoZaTuFiguru(String position) {
+        if (Math.abs(ChessPiece.slovnaKoordinata(position) - ChessPiece.slovnaKoordinata(this.position)) !=
+                Math.abs(ChessPiece.brojevnaKoordinata(position) - ChessPiece.brojevnaKoordinata(this.position)))
+            return false;
         return true;
     }
 
