4d4ddd607fbfae1c890cb3aad7010278f9b9c10d
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-08 01:53:23 +0100

    Rook provjera uradjena

--- src/ba/unsa/etf/rpr/Rook.java
+++ src/ba/unsa/etf/rpr/Rook.java
@@ -3,6 +3,9 @@ package ba.unsa.etf.rpr;
 public class Rook extends ChessPiece {
 
     protected boolean ispravnoZaTuFiguru(String position) {
+        if ((Math.abs(ChessPiece.slovnaKoordinata(position) - ChessPiece.slovnaKoordinata(this.position)) != 0) &&
+                (Math.abs(ChessPiece.brojevnaKoordinata(position) - ChessPiece.brojevnaKoordinata(this.position)) != 0))
+            return false;
         return true;
     }
 
