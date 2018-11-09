26c55d88e72f157ebc9b9dcee1315267a66843df
Dzavid <dbrdar1@etf.unsa.ba>
2018-11-09 02:46:52 +0100

    Queen provjera dodana

--- src/ba/unsa/etf/rpr/Queen.java
+++ src/ba/unsa/etf/rpr/Queen.java
@@ -3,7 +3,11 @@ package ba.unsa.etf.rpr;
 public class Queen extends ChessPiece {
 
     protected boolean legalForThatKindOfPiece(String position) {
-
+        if ((Math.abs(ChessPiece.letterCoordinate(position) - ChessPiece.letterCoordinate(this.position)) != 0) &&
+                (Math.abs(ChessPiece.numberCoordinate(position) - ChessPiece.numberCoordinate(this.position)) != 0) &&
+                (Math.abs(ChessPiece.letterCoordinate(position) - ChessPiece.letterCoordinate(this.position)) !=
+                Math.abs(ChessPiece.numberCoordinate(position) - ChessPiece.numberCoordinate(this.position))))
+            return false;
         return true;
     }
 
